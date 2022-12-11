e2d910ce866ce5744766c0858b5009f06e3ff542
ScrewUcaptain <basilebrusseau@gmail.com>
2022-12-11 18:26:01 +0100

    Updates

---
+++ precache-manifest.31822f7b94a739971d633e4b51f9ee26.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c063c576c4e920e7b59",
+    "url": "/tenzies/static/js/main.8c063c57.chunk.js"
+  },
+  {
+    "revision": "2fe7427ac14bcf503c1d",
+    "url": "/tenzies/static/js/1.2fe7427a.chunk.js"
+  },
+  {
+    "revision": "9a1b99c3c42426eb0535",
+    "url": "/tenzies/static/js/runtime~main.9a1b99c3.js"
+  },
+  {
+    "revision": "cd9a745032c8887f09d25ac866d0a464",
+    "url": "/tenzies/index.html"
+  }
+];
